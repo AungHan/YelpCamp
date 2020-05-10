4f118b92fff6689f537af167e7cf4229799fa66c
Aung Han <agghan@gmail.com>
2020-05-10 19:12:50 +0800

    Update YelpCamp
    - refactored modules
    - styling with Bootstrap
    - nested routing for comments
    - associating Comments and Campgrounds model in db

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
+const Comment = require("./models/comment");
+const seedDb = require("./seed");
 const app = express();
 
 const PORT = 3000;
-
+//seedDb();
 // application configurations
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // db configurations
@@ -16,7 +18,6 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", {
     useUnifiedTopology: true,
     useNewUrlParser: true
 });
-
 
 // Routings
 app.get("/", (req, res) => {
@@ -29,13 +30,13 @@ app.get("/campgrounds", (req, res) => {
         if(err){
             console.log(err);
         }else{
-            res.render("index", { campgrounds: allCampgrounds });
+            res.render("campgrounds/index", { campgrounds: allCampgrounds });
         }
     });
 });
 
 app.get("/campgrounds/new", (req, res) => {
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 app.post("/campgrounds", (req, res) => {
@@ -56,12 +57,49 @@ app.post("/campgrounds", (req, res) => {
 
 app.get("/campgrounds/:id", (req, res) => {
     // find campground with provided id
-    Campground.findById(req.params.id, (err, foundCampground) => {
+    Campground.findById(req.params.id)
+        .populate("comments")
+        .exec((err, foundCampground) => {
         if(err){
             console.log(err);
         }else{
-            res.render("show", { campground: foundCampground});
+            res.render("campgrounds/show", { campground: foundCampground});
         }
+    });
+});
+
+// ======================
+// COMMENTS ROUTES
+// ======================
+
+app.get("/campgrounds/:id/comments/new", (req, res) => {
+    Campground.findById(req.params.id, (err, campground) => {
+        if(err){
+            console.log(err);
+        }    else{
+            res.render("comments/new", { campground: campground });
+        }
+    });
+
+});
+
+app.post("/campgrounds/:id/comments", (req, res) => {
+    Campground.findById(req.params.id, (err, campground) => {
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       } else{
+           // create new comment
+           Comment.create(req.body.comment, (err, newComment) => {
+               if(err){
+                   console.log(err);
+               }else{
+                   campground.comments.push(newComment);
+                   campground.save();
+                   res.redirect("/campgrounds/" + campground._id);
+               }
+           });
+       }
     });
 });
 
---
+++ seed.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+const Comment = require("./models/comment");
+
+const data = [
+    {
+        name: "Cloud's Rest",
+        image: "https://pixabay.com/get/52e3d5404957a514f1dc84609620367d1c3ed9e04e5074417c2f78dc924ac2_340.jpg",
+        description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+    },
+    {
+        name: "Morning pot",
+        image: "https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852547940752c73d1904a_340.jpg",
+        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32."
+    },
+    {
+        name: "Flourish",
+        image: "https://pixabay.com/get/52e3d3404a55af14f1dc84609620367d1c3ed9e04e5074417c2f78dc924ac2_340.png",
+        description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+    }
+];
+
+function seedDB(){
+    // removed all campgrounds
+    Campground.deleteMany({}, function(err){
+       if(err){
+           console.log(err);
+       }
+       console.log("removed campground!");
+        // add some campgrounds
+        data.forEach(seed => {
+            Campground.create(seed, (err, newOne) => {
+                if(err){
+                    console.log(err);
+                }else{
+                    Comment.create({
+                        text: "I think therefore I am!",
+                        author: "Elon Musk"
+                    }, (err, newComment) => {
+                        if(err){
+                            console.log(err);
+                        }else{
+                            newOne.comments.push(newComment);
+                            newOne.save();
+                        }
+                    });
+                }
+            });
+        });
+    });
+
+}
+
+module.exports = seedDB;
