30a282313d57eae5456e04856ee7743b0d5a198b
Aung Han <agghan@gmail.com>
2020-05-11 12:00:13 +0800

    Update Yelp Camp
    - added user authentication
    - sign up and log in form

--- app.js
+++ app.js
@@ -4,19 +4,41 @@ const mongoose = require("mongoose");
 const Campground = require("./models/campground");
 const Comment = require("./models/comment");
 const seedDb = require("./seed");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const passportLocalStrategy = require("passport-local-mongoose");
+const User = require("./models/user");
 const app = express();
 
 const PORT = 3000;
 //seedDb();
-// application configurations
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // db configurations
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {
     useUnifiedTopology: true,
     useNewUrlParser: true
+});
+
+// application configurations
+app.set("view engine", "ejs");
+app.use(require("express-session")({
+    secret: "yelppy",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// middleware
+app.use((req, res, next) => {
+   res.locals.currentUser = req.user;
+   next();
 });
 
 // Routings
@@ -30,16 +52,19 @@ app.get("/campgrounds", (req, res) => {
         if(err){
             console.log(err);
         }else{
-            res.render("campgrounds/index", { campgrounds: allCampgrounds });
+            res.render("campgrounds/index", {
+                campgrounds: allCampgrounds,
+                currentUser: req.user
+            });
         }
     });
 });
 
-app.get("/campgrounds/new", (req, res) => {
+app.get("/campgrounds/new", isLoggedIn, (req, res) => {
     res.render("campgrounds/new");
 });
 
-app.post("/campgrounds", (req, res) => {
+app.post("/campgrounds", isLoggedIn, (req, res) => {
     Campground.create({
         name: req.body.campName,
         image: req.body.campImage,
@@ -72,7 +97,7 @@ app.get("/campgrounds/:id", (req, res) => {
 // COMMENTS ROUTES
 // ======================
 
-app.get("/campgrounds/:id/comments/new", (req, res) => {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, (req, res) => {
     Campground.findById(req.params.id, (err, campground) => {
         if(err){
             console.log(err);
@@ -83,7 +108,7 @@ app.get("/campgrounds/:id/comments/new", (req, res) => {
 
 });
 
-app.post("/campgrounds/:id/comments", (req, res) => {
+app.post("/campgrounds/:id/comments", isLoggedIn, (req, res) => {
     Campground.findById(req.params.id, (err, campground) => {
        if(err){
            console.log(err);
@@ -102,6 +127,52 @@ app.post("/campgrounds/:id/comments", (req, res) => {
        }
     });
 });
+
+// AUTH ROUTE
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    User.register(new User({ username: req.body.username }),
+        req.body.password,
+        (err, user) => {
+            if(err){
+                console.log(err);
+                return res.redirect("/register");
+            }
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/campgrounds");
+            });
+        });
+});
+
+// show log in form
+app.get("/login", (req, res) => {
+   res.render("login");
+});
+
+app.post("/login",
+    passport.authenticate("local", {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }),
+    (req, res) => {
+
+});
+
+// logout
+app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/campgrounds");
+})
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(PORT, () => {
     console.log("server started at localhost:" + PORT);
